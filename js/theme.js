0d007b58f0f18917a4776e8ae4ba4f7d1831dace
Jamie Ritchie <jamie@property-network.co.uk>
2019-07-23 12:06:49 +0100

    Bigger theme button, show toast notification on theme switch

--- js/theme.js
+++ js/theme.js
@@ -6,12 +6,23 @@ function selectTheme() {
     confirmButtonColor: '#00e3c6',
     cancelButtonColor: '#db0000',
     confirmButtonText: 'Blue',
-    cancelButtonText: 'Red'
+    cancelButtonText: 'Red',
+    allowOutsideClick: false,
   }).then((r) => {
     if (r.value) {
       document.getElementById('currentCSS').href = "css/master-blue.css";
+      success.fire({ title: "Blue Theme Selected!" });
     } else {
       document.getElementById('currentCSS').href = "css/master-red.css";
+      success.fire({ title: "Red Theme Selected!" });
     }
   });
 }
+
+const success = Swal.mixin({
+    type: 'success',
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 1500,
+    toast: true
+});
